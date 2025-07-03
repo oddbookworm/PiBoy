7b7d0e1431de2d24900de92c6f7b38fb3e0bd48a
Andrew Coffey <andrewryancoffey@hotmail.com>
2025-07-02 20:32:02 -0500

    Perks schema

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ xmlschema = etree.XMLSchema(xmlschema_doc)
 doc = etree.parse("configs/Items.xml")
 if not xmlschema.validate(doc):
     print(xmlschema.error_log)
+
+xmlschema_doc = etree.parse("configs/Perks.xsd")
+xmlschema = etree.XMLSchema(xmlschema_doc)
+doc = etree.parse("configs/Perks.xml")
+if not xmlschema.validate(doc):
+    print(xmlschema.error_log)
